f065a3e0d2d19f7065077ccdf6bcc326ceb330a6
Ahmed-Adel0 <adla03861@gmail.com>
2023-10-06 15:46:34 +0300

    Implement auto-save functionality with localStorage

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# live-web-code-editor
+# Code Editor with Auto-Save Functionality
 
-hello there, your task is to implement auto-save functionality with localStorage, and auto-load the saved code when user revisits or refreshes the page.
+This repository contains a code editor application that allows users to write HTML, CSS, and JavaScript code and preview the output in real-time.
+The main objective of this project is to implement auto-save functionality using the browser's localStorage API, which automatically saves the user's code and reloads it when the user revisits or refreshes the page.
 
-fork the repository and start working on it.
+## Implementation Details
 
-best of luck.
+The auto-save functionality is implemented using JavaScript and the localStorage API provided by the browser.
+Here's a breakdown of the implementation:
+
+- The code editor interface includes separate tabs for HTML, CSS, and JavaScript code.
+
+- Whenever the user makes changes in the code editor, the `showPreview(editorId)` function is called.
+  This function retrieves the current code from the editor and stores it in localStorage with a unique editorId.
+
+- The `updatePreview(frame, htmlCode, cssCode, jsCode)` function is responsible for updating the preview window.
+  It receives the frame (document) of the preview window, as well as the current HTML, CSS, and JavaScript code.
+  It opens the frame, writes the combined code to the frame, and then closes it.
+
+- When the window is loaded, the `window.addEventListener("load", ...)` function is triggered.
+  It retrieves the saved code from localStorage (if available) or initializes it with default values.
+  The code editor and preview window are then updated with the loaded code.
+
+- Additionally, the code is automatically saved to localStorage whenever modifications are made.
+  This ensures that the latest changes are persisted even if the user closes the browser or navigates away from the page.
+
+Happy coding!
--- js/app.js
+++ js/app.js
@@ -1,37 +1,57 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+window.addEventListener("load", () => {
+  const htmlCode = localStorage.getItem("htmlCode") || "<div>\n\n</div>";
+  const cssCode = localStorage.getItem("cssCode") || "<style>\n\n</style>";
+  const jsCode = localStorage.getItem("jsCode") || "<script>\n\n</script>";
+  const frame =
+    document.getElementById("preview-window").contentWindow.document;
 
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = ""+document.getElementById("cssCode").value+"";
-    var jsCode = ""+document.getElementById("jsCode").value+"";
-    var frame = document.getElementById("preview-window").contentWindow.document;
-    frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
-    frame.close();
+  document.getElementById("htmlCode").value = htmlCode;
+  document.getElementById("cssCode").value = cssCode;
+  document.getElementById("jsCode").value = jsCode;
+
+  updatePreview(frame, htmlCode, cssCode, jsCode);
+});
+
+function showPreview(editorId) {
+  const htmlCode = document.getElementById("htmlCode").value;
+  const cssCode = document.getElementById("cssCode").value;
+  const jsCode = document.getElementById("jsCode").value;
+  const frame =
+    document.getElementById("preview-window").contentWindow.document;
+
+  const code = document.getElementById(editorId).value; // input value
+  localStorage.setItem(editorId, code); // add value to local Storage
+
+  updatePreview(frame, htmlCode, cssCode, jsCode);
 }
 
-function show(x){
-    document.getElementById("html").style.display="none";
-    document.getElementById("css").style.display="none";
-    document.getElementById("js").style.display="none";
-    document.getElementById("result").style.display="none";
-    document.getElementById(x).style.display="block";
+function updatePreview(frame, htmlCode, cssCode, jsCode) {
+  frame.open();
+  frame.write(htmlCode + cssCode + jsCode);
+  frame.close();
 }
 
-function show_all(){
-    if(window.innerWidth>=992)
-    {
-        document.getElementById("html").style.display="block";
-        document.getElementById("css").style.display="block";
-        document.getElementById("js").style.display="block";
-        document.getElementById("result").style.display="block";
-    }
-    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
-    {
-        document.getElementById("css").style.display="none";
-        document.getElementById("js").style.display="none";
-        document.getElementById("result").style.display="none";
-    }
+function show(x) {
+  const elements = ["html", "css", "js", "result"];
+  elements.forEach((element) => {
+    document.getElementById(element).style.display = "none";
+  });
+  document.getElementById(x).style.display = "block";
+}
+
+function show_all() {
+  if (window.innerWidth >= 992) {
+    const elements = ["html", "css", "js", "result"];
+    elements.forEach((element) => {
+      document.getElementById(element).style.display = "block";
+    });
+  }
+  if (
+    window.innerWidth < 992 &&
+    document.getElementById("html").style.display == "block"
+  ) {
+    document.getElementById("css").style.display = "none";
+    document.getElementById("js").style.display = "none";
+    document.getElementById("result").style.display = "none";
+  }
 }
